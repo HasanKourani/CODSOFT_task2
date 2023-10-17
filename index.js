08e11adf48b0fcf5913574c2fa702c2415844f7d
Hasan Kourani <hasankoura8@gmail.com>
2023-10-17 20:06:10 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var typed = new Typed("#element", {
+  strings: ["MOHAMMAD HASAN KOURANI"],
+  typeSpeed: 150,
+  backSpeed: 50,
+  loop: true,
+});
+
+var typed = new Typed("#element1", {
+  strings: ["FULL-STACK WEB DEVELOPER"],
+  typeSpeed: 130,
+  backSpeed: 50,
+  loop: true,
+});
+
+document.getElementById("year").innerHTML = new Date().getFullYear();
